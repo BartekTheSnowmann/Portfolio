7df419a9c75643f587bec0828769d4eb23877537
HenryBalon <115709320+HenryBalon@users.noreply.github.com>
2023-04-29 23:50:55 +0200

    small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,15 +10,13 @@ function Footer() {
           </div>
           <div className='flex gap-x-2'>
             <div>
-              <AiFillGithub size={40} className='cursor-pointer'/>
               <a href='https://github.com/bartekthesnowmann'>
-                <p className='text-gray-400 cursor-pointer'></p>
+                <AiFillGithub size={40} className='cursor-pointer'/>
               </a>
             </div>
             <div>
-              <AiFillLinkedin size={40} className='cursor-pointer text-[#0072b1] '/>
               <a href='https://www.linkedin.com/in/bartosz-mr%C3%B3z-536710274/'>
-                <p className='text-gray-400 cursor-pointer'></p>
+                <AiFillLinkedin size={40} className='cursor-pointer text-[#0072b1] '/>
               </a>
             </div>
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,7 @@ function Navbar() {
 
   return (
     <div className='h-20'>
-        <motion.div className='drop-shadow-md z-10 fixed h-20 bg-white text-secondary w-full flex justify-center items-center px-4'
-        initial={{y:-100, opacity:0}}
-        animate={{y:0, opacity:1}}
-        transition={{delay:2,duration:0.6, type:'tween'}}>
+        <div className='drop-shadow-md z-10 fixed h-20 bg-white text-secondary w-full flex justify-center items-center px-4'>
             <ul className='flex gap-x-4 text-lg'>
                 {['Home', 'About', 'Projects'].map((item)=>
                 (
@@ -17,7 +14,7 @@ function Navbar() {
                     </li>
                 ))}
             </ul>
-        </motion.div>
+        </div>
     </div>
   )
 }
